e891e7fcf9a29fee216848f9696b1b1bfc98d808
Tejesh Chauragade <chauragade.tejesh@gmail.com>
2019-09-27 15:54:50 +0530

    Added new endpoint "feature/"!

--- demoapp/userapp/urls.py
+++ demoapp/userapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, re_path
-from .views import HomeView
+from .views import (
+    HomeView,
+    FeatureView
+)
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('api-token-auth/', obtain_auth_token, name='api_auth_token')
+    path('api-token-auth/', obtain_auth_token, name='api_auth_token'),
+    path('feature/', FeatureView.as_view(), name='feature'),
 ]
